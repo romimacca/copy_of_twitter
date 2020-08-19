26d459e970d0ba7147d9f155476857dd7824b008
Romi Maccarini <romi@clay.cl>
2020-08-18 20:21:05 -0400

    routes devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
+    before_action :configure_permited_parameters, if: :devise_controller?
+
+    protected
+    def configure_permited_parameters
+        devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :photo_url])
+    end
 end
